dabad3eb068c63589b1d65094ecd866f13826476
Kunj-galledar <84000594+Kunj-galledar@users.noreply.github.com>
2022-03-11 20:49:50 +0530

    update

--- assets/js/wizard.js
+++ assets/js/wizard.js
@@ -1,52 +1,49 @@
+// $(document).ready(function () {
+//     // hidden things
+//     $(".form-business").hide();
+
+//     // next button
+//     $(".next").on({
+//         click: function () {
+//             $("#myTab").find(".active").next().addClass("active");
+//             $(this).parents(".tab-pane").fadeOut("slow", function () {
+//                 $(this).next(".tab-pane").fadeIn("slow");
+//             });
+//         }
+//     });
+
+//     // back button
+//     $(".back").on({
+//         click: function () {
+//             $("#myTab .nav-item .nav-link.active").last().removeClass("active");
+//             $(this).parents(".tab-pane").fadeOut("slow", function () {
+//                 $(this).prev(".tab-pane").fadeIn("slow");
+//             });
+//         }
+//     });
+// });
+
+
+
 $(document).ready(function () {
     // hidden things
     $(".form-business").hide();
-    $("#successMessage").hide();
     // next button
-    // $(".next").on({
-    //     click: function () {
-    //         // select any card
-    //         var getValue = $(this).parents(".row").find(".card").hasClass("active-card");
-    //         if (getValue) {
-    //             $("#progressBar").find(".active").next().addClass("active");
-    //             $("#alertBox").addClass("d-none");
-    //             $(this).parents(".row").fadeOut("slow", function () {
-    //                 $(this).next(".row").fadeIn("slow");
-    //             });
-
-    //         } else {
-    //             $("#alertBox").removeClass("d-none");
-    //         }
-    //     }
-    // });
-    // back button
-    $(".back").on({
+    $(".proceed-btn").on({
         click: function () {
-            $("#progressBar .active").last().removeClass("active");
-            $(this).parents(".row").fadeOut("slow", function () {
-                $(this).prev(".row").fadeIn("slow");
+            $("#myTab").find(".active").parent().next().find('.nav-link').addClass("active");
+            $(this).parents(".tab-pane").fadeOut("slow", function () {
+                $(this).next(".tab-pane").fadeIn("slow");
             });
         }
     });
-    //finish button
-    $(".submit-button").on({
+    // back button
+    $(".backward-btn").on({
         click: function () {
-            $("#wizardRow").fadeOut(300);
-            $(this).parents(".row").children("#successForm").fadeOut(300);
-            $(this).parents(".row").children("#successMessage").fadeIn(3000);
-        }
-    });
-    //Active card on click function
-    $(".card").on({
-        click: function () {
-            $(this).toggleClass("active-card");
-            $(this).parent(".col").siblings().children(".card").removeClass("active-card");
-        }
-    });
-    //back to wizard
-    $(".back-to-wizard").on({
-        click: function () {
-            location.reload(true);
+            $("#myTab .active").parent().last().find('.nav-link').removeClass("active");
+            $(this).parents(".tab-pane").fadeOut("slow", function () {
+                $(this).prev(".tab-pane").fadeIn("slow");
+            });
         }
     });
 });
