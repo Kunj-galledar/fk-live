2eaeeaa98b26f6cf167e126d9e878a7c35fea675
krupasavadiya <47918857+krupasavadiya@users.noreply.github.com>
2022-04-13 14:23:20 +0530

    update

--- fast-2/assets/js/script.js
+++ fast-2/assets/js/script.js
@@ -355,3 +355,21 @@ $(document).ready(function () {
         return false;
     });
 });
+
+/*=====================
+        02. dropdown category function js
+    ==========================*/
+$(function () {
+    var list = $(".js-dropdown-list");
+    var link = $(".js-link");
+    link.click(function (e) {
+        e.preventDefault();
+        list.slideToggle(200);
+    });
+    list.find("li").click(function () {
+        var text = $(this).html();
+        var icon = '<i class="fa fa-chevron-down"></i>';
+        link.html(text + icon);
+        list.slideToggle(200);
+    });
+});
