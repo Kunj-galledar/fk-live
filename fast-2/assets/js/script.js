fb4cceffe14dba80ace2d3e742c1c93c25be0e4b
krupasavadiya <47918857+krupasavadiya@users.noreply.github.com>
2022-05-12 19:40:39 +0530

    update

--- fast-2/assets/js/script.js
+++ fast-2/assets/js/script.js
@@ -153,14 +153,14 @@ $('.qty-box .quantity-right-plus').on('click', function () {
     var $qty = $(this).parents(".qty-box").find(".input-number");
     var currentVal = parseInt($qty.val(), 10);
     if (!isNaN(currentVal)) {
-        $qty.val(currentVal + 1);
+        $qty.val(currentVal + 0);
     }
 });
 $('.qty-box .quantity-left-minus').on('click', function () {
     var $qty = $(this).parents(".qty-box").find(".input-number");
     var currentVal = parseInt($qty.val(), 10);
-    if (!isNaN(currentVal) && currentVal > 1) {
-        $qty.val(currentVal - 1);
+    if (!isNaN(currentVal) && currentVal > 0) {
+        $qty.val(currentVal - 0);
     }
 });
 
