9210445104c4632c833f84ce526f4b05088b25fb
Kunj-galledar <84000594+Kunj-galledar@users.noreply.github.com>
2022-07-20 18:01:19 +0530

    update

---
+++ fast-new/assets/js/top-filter.js
@@ -0,0 +1,8 @@
+ /**=====================
+     Top Filter js
+==========================**/
+ $(document).ready(function () {
+     $(".sidebar-filter-menu").click(function () {
+         $(".top-filter-category").toggleClass("show");
+     });
+ });
