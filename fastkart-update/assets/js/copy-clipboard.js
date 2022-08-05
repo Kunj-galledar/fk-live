71ac31047449597c7a1b9d06c8721bc62ff86704
Kunj-galledar <84000594+Kunj-galledar@users.noreply.github.com>
2022-08-06 01:55:24 +0530

    update

--- fastkart-update/assets/js/copy-clipboard.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-    "use strict";
-    var clipboard = new ClipboardJS('.bank-coupon');
-    clipboard.on('success', function (e) {
-        e.clearSelection();
-    });
-    clipboard.on('error', function (e) {});
-})(jQuery);
