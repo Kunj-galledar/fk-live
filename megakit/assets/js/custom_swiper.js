3fa8bf641bb8b49f760c49794eb987d57d99faca
Kunj Galledar <84000594+Kunj-galledar@users.noreply.github.com>
2022-11-19 13:15:16 +0530

    update

---
+++ megakit/assets/js/custom_swiper.js
@@ -0,0 +1,19 @@
+ /**=====================
+     Custom Swiper js
+==========================**/
+ var swiper = new Swiper(".slider-1", {
+     slidesPerView: 1,
+     spaceBetween: 12,
+     centeredSlides: false,
+     slidesPerGroup: 1,
+     //  loop: true,
+     //  loopFillGroupWithBlank: true,
+     //  pagination: {
+     //      el: ".swiper-pagination",
+     //      clickable: true,
+     //  },
+     //  navigation: {
+     //      nextEl: ".swiper-button-next",
+     //      prevEl: ".swiper-button-prev",
+     //  },
+ });
---
+++ megakit/assets/js/script.js
@@ -0,0 +1,7 @@
+// Active Button Js
+// $(document).ready(function () {
+//     $('category-section .category-list li').click(function () {
+//         $('li').removeClass("active");
+//         $(this).addClass("active");
+//     });
+// });
