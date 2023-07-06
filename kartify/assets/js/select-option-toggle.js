2304c1bc34e8d80d8e5449dadae65f77d9b2eae4
Kunj Galledar <84000594+Kunj-galledar@users.noreply.github.com>
2023-07-06 18:49:40 +0530

    update

--- kartify/assets/js/select-option-toggle.js
+++ kartify/assets/js/select-option-toggle.js
@@ -1,7 +1,23 @@
-document.querySelector(".select-btn").addEventListener("click", function () {
-    document.querySelector(".select-option-box").classList.add("show");
-});
+// document.querySelector(".select-btn").addEventListener("click", function () {
+//     document.querySelector(".select-option-box").classList.add("show");
+// });
 
-function closeSidebar() {
-    document.querySelector(".select-option-box").classList.remove("show");
-}
+// function closeSidebar() {
+//     document.querySelector(".select-option-box").classList.remove("show");
+// }
+
+// var buttons = document.querySelectorAll('.select-btn');
+// var element = document.querySelector('.select-option-box');
+// var close = document.querySelector('.close-btn');
+
+// buttons.forEach(function (button) {
+//     button.addEventListener('click', function () {
+//         element.classList.add('show');
+//     });
+// });
+
+// buttons.forEach(function (button) {
+//     button.addEventListener('click', function () {
+//         element.classList.remove('show');
+//     });
+// });
