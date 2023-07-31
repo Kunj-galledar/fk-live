d6f6d13cd71d0b7ce235d703458f60decc82f0d3
Kunj Galledar <84000594+Kunj-galledar@users.noreply.github.com>
2023-07-31 18:28:24 +0530

    update

--- voxo/front-end/assets/js/cart_modal_resize.js
+++
@@ -1,8 +0,0 @@
-/**=====================
-     Cart Modal Resize js
-==========================**/
-var myModal = document.getElementById("addtocart");
-
-myModal.addEventListener("shown.bs.modal", function () {
-  $(window).trigger("resize");
-});
