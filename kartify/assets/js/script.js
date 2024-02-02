b2fbf06faff7120a75f8efc8931cdd68757ece60
Kunj Galledar <84000594+Kunj-galledar@users.noreply.github.com>
2024-02-02 18:47:04 +0530

    update

--- kartify/assets/js/script.js
+++ kartify/assets/js/script.js
@@ -141,47 +141,47 @@ window.addEventListener('blur', function () {
 //     footerContain.style.display = "block";
 // }
 
-var notifiWishlist = document.querySelector(".notifi-wishlist");
-notifiWishlist.addEventListener("click", function () {
-    var options = {
-        icon: "fa fa-check",
-        title: "Success!",
-        message: "Item Successfully added in wishlist",
-    };
-    var settings = {
-        element: "body",
-        position: null,
-        type: "info",
-        allow_dismiss: true,
-        newest_on_top: false,
-        showProgressbar: true,
-        placement: {
-            from: "top",
-            align: "right",
-        },
-        offset: 20,
-        spacing: 10,
-        z_index: 1031,
-        delay: 5000,
-        animate: {
-            enter: "animated fadeInDown",
-            exit: "animated fadeOutUp",
-        },
-        icon_type: "class",
-        template: '<div data-notify="container" class="col-xxl-3 col-lg-5 col-md-6 col-sm-7 col-12 alert alert-{0}" role="alert">' +
-            '<button type="button" aria-hidden="true" class="btn-close" data-notify="dismiss"></button>' +
-            '<span data-notify="icon"></span> ' +
-            '<span data-notify="title">{1}</span> ' +
-            '<span data-notify="message">{2}</span>' +
-            '<div class="progress" data-notify="progressbar">' +
-            '<div class="progress-bar progress-bar-info progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
-            "</div>" +
-            '<a href="{3}" target="{4}" data-notify="url"></a>' +
-            "</div>",
-    };
+// var notifiWishlist = document.querySelector(".notifi-wishlist");
+// notifiWishlist.addEventListener("click", function () {
+//     var options = {
+//         icon: "fa fa-check",
+//         title: "Success!",
+//         message: "Item Successfully added in wishlist",
+//     };
+//     var settings = {
+//         element: "body",
+//         position: null,
+//         type: "info",
+//         allow_dismiss: true,
+//         newest_on_top: false,
+//         showProgressbar: true,
+//         placement: {
+//             from: "top",
+//             align: "right",
+//         },
+//         offset: 20,
+//         spacing: 10,
+//         z_index: 1031,
+//         delay: 5000,
+//         animate: {
+//             enter: "animated fadeInDown",
+//             exit: "animated fadeOutUp",
+//         },
+//         icon_type: "class",
+//         template: '<div data-notify="container" class="col-xxl-3 col-lg-5 col-md-6 col-sm-7 col-12 alert alert-{0}" role="alert">' +
+//             '<button type="button" aria-hidden="true" class="btn-close" data-notify="dismiss"></button>' +
+//             '<span data-notify="icon"></span> ' +
+//             '<span data-notify="title">{1}</span> ' +
+//             '<span data-notify="message">{2}</span>' +
+//             '<div class="progress" data-notify="progressbar">' +
+//             '<div class="progress-bar progress-bar-info progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+//             "</div>" +
+//             '<a href="{3}" target="{4}" data-notify="url"></a>' +
+//             "</div>",
+//     };
 
-    $.notify(options, settings);
-});
+//     $.notify(options, settings);
+// });
 
 /*=====================
     header Dropdown Js
@@ -200,7 +200,7 @@ notifiWishlist.addEventListener("click", function () {
 
 /*=====================
     Switch Authentication Js
-==========================*/ 
+==========================*/
 let myPopup = document.querySelector(".login-box");
 let btnClose = document.querySelector(".next-button");
 let myPopup2 = document.querySelector(".signup-box");
